03e71b479f21dbde31f7ee3cd50b1b0d84bccff6
4equest <107108812+4equest@users.noreply.github.com>
2024-03-06 00:25:08 +0900

    upload-core (#4)
    init

---
+++ core/src/image/azure_services.py
@@ -0,0 +1,10 @@
+# azure_services.py
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.ai.translation.text import TextTranslationClient, TranslatorCredential
+
+class AzureServices:
+    def __init__(self, vision_key, vision_endpoint, translator_key, translator_endpoint):
+        vision_credentials = CognitiveServicesCredentials(vision_key)
+        self.vision_client = ComputerVisionClient(vision_endpoint, vision_credentials)
+        self.translator_client = TextTranslationClient(credential=TranslatorCredential(translator_key, "japaneast"))
