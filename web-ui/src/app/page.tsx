5a7383e3f0177c3a7a8aa989bce5e7280d8757f5
darallium <annihilation0504@gmail.com>
2024-03-06 05:17:16 +0900

    created empty project

---
+++ web-ui/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import * as mui from '@material-ui/core';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="App">
+      empty
+      </div>
+    </main>
+  );
+}
